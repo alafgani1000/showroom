974d83e9724714b32784900a17aa727fd9f5a910
alafgani1000 <irhabyaganal625@gmail.com>
2019-12-05 07:51:27 +0700

    create todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controller\Todo;
+use App\Http\Controller\TodoAttachment;
+use App\Http\Controller\TodoDetail;
 
 class TodoController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
-        
+        Todo::create([
+            'name' => $request->name,
+            'plan_finish_date' => $request->plan_finish_date,
+            'date_of_take' => $request->date_of_take,
+        ]);
     }
+
+    
 }

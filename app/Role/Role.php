992a66347be12b938056e39b1ac1c82f4798fe62
alafgani1000 <irhabyaganal625@gmail.com>
2020-01-15 14:09:18 +0700

    role facade

--- app/Role/Role.php
+++ app/Role/Role.php
@@ -1,15 +1,21 @@
 <?php
 
 namespace App\Role;
+use App\Role as RoleApp;
 
 class Role
 {
     public function hasRole($param)
     {
-
+        return RoleApp::find($param);
     }
 
     public function hasRoles(array $param)
+    {
+        return RoleApp::whereIn('id',$param);
+    }
+
+    public function getRoles()
     {
         
     }
